d25ac1457a99569cac62ffdfdebc2a837d61a7b6
FRANÇOISE Anthonia <hei.anthonia@gmail.com>
2026-08-11 13:55:49 +0300

    feat: add createStudent, updateStudent

--- src/service/StudentService.ts
+++ src/service/StudentService.ts
@@ -1,4 +1,5 @@
 import {StudentRepository} from "../repository/StudentRepository";
+import {CreateStudent, UpdateStudent} from "../model/Student";
 
 export class StudentService {
     private studentRepository: StudentRepository;
@@ -15,9 +16,24 @@ export class StudentService {
         if (!student){throw new Error("student not found")};
       return student;
     }
+
+    async createStudent (data:CreateStudent){
+        return this.studentRepository.create(data);
+    }
+
+    async updateStudent (id:number,data:UpdateStudent){
+        const student = await  this.studentRepository.findById(id);
+        if (!student){
+            throw new Error('student pas trouvé');
+        }
+
+        return this.studentRepository.update(id,data);
+    }
     async deleteStudent (id:number){
         const student = await this.studentRepository.findById(id);
-        if (!student){throw new Error("student not found")};
+        if (!student){
+            throw new Error("student not found");
+        }
         await this.studentRepository.delete(id);
         return student;
     }
