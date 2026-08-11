bc2c8c6e95b8482f9b14ea8724719301dbea2b2c
FRANÇOISE Anthonia <hei.anthonia@gmail.com>
2026-08-11 11:22:09 +0300

    feat: add getStudentAll, getStudentById, deleteStudent in StudentService

---
+++ src/service/StudentService.ts
@@ -0,0 +1,24 @@
+import {StudentRepository} from "../repository/StudentRepository";
+
+export class StudentService {
+    private studentRepository: StudentRepository;
+
+    constructor(studentRepository: StudentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    async getAllStudents (){
+        return this.studentRepository.findAll();
+    }
+    async getStudentsById (id:number){
+        const student = this.studentRepository.findById(id);
+        if (!student){throw new Error("student not found")};
+      return student;
+    }
+    async deleteStudent (id:number){
+        const student = this.studentRepository.findById(id);
+        if (!student){throw new Error("student not found")};
+        await this.studentRepository.delete(id);
+        return student;
+    }
+}
