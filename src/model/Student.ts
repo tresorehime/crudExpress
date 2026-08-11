c3601d1340ae2d37fc32ad64e13d7ee9877b3604
FRANÇOISE Anthonia <hei.anthonia@gmail.com>
2026-08-11 12:56:18 +0300

    feat: add createStudent, updateStudent in model

--- src/model/Student.ts
+++ src/model/Student.ts
@@ -1,7 +1,19 @@
 export interface Student {
-    id:string,
-    firstName:string,
-    lastName:string,
-    mail:string,
-    phone:string
+    id: string,
+    firstName: string,
+    lastName: string,
+    mail: string,
+    phone: string
+}
+
+export interface CreateStudent {
+    firstName: string,
+    lastName: string,
+    mail: string
+}
+
+export interface UpdateStudent {
+    firstName?: string,
+    lastName?: string,
+    mail?: string
 }
