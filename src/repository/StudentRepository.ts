3797e435d90874f6dec0a59e5fdfce7ff24d22a5
FRANÇOISE Anthonia <hei.anthonia@gmail.com>
2026-08-11 11:11:13 +0300

    refactor:rename studentRepository to StudentRepository

--- src/repository/StudentRepository.ts
+++ src/repository/StudentRepository.ts
@@ -1,6 +1,6 @@
 import pool from '../config/db';
 
-export class studentRepository {
+export class StudentRepository {
     async findAll (){
         const result = await pool.query('SELECT * FROM student');
         return result.rows;
