b941152d9e5b8fe6a5cd5aa9727080fa7460f362
FRANÇOISE Anthonia <hei.anthonia@gmail.com>
2026-08-11 18:16:28 +0300

    fix:parse JSON request body

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import studentsRoutes from "./routes/studentsRoutes";
+import studentsRoutes from "./routes/StudentsRoutes";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
 
 app.use('/crud', studentsRoutes );
 
