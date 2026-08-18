e453fc85ede6d3446011d5be491077ed2b229a6a
FRANÇOISE Anthonia <hei.anthonia@gmail.com>
2026-08-18 22:10:17 +0300

    feat: protect student routes

--- src/routes/StudentsRoutes.ts
+++ src/routes/StudentsRoutes.ts
@@ -2,6 +2,7 @@ import {Router} from "express";
 import {StudentRepository} from "../repository/StudentRepository";
 import {StudentService} from "../service/StudentService";
 import {StudentController} from "../controller/StudentController";
+import { authMiddleware } from "../middleware/authMiddleware";
 
 const router = Router();
 
@@ -9,12 +10,12 @@ const studentRepository = new StudentRepository();
 const studentService = new StudentService(studentRepository);
 const studentController = new StudentController(studentService);
 
-router.get('/students', studentController.getAllStudents.bind(studentController));
-router.get('/students/:id', studentController.getStudentsById.bind(studentController));
-router.post('/students', studentController.createStudent.bind(studentController));
-router.put('/students/:id', studentController.updateStudent.bind(studentController));
-router.patch('/students/:id', studentController.updateStudent.bind(studentController));
-router.delete('/students/:id', studentController.deleteStudent.bind(studentController));
+router.get('/students',authMiddleware, studentController.getAllStudents.bind(studentController));
+router.get('/students/:id',authMiddleware, studentController.getStudentsById.bind(studentController));
+router.post('/students',authMiddleware, studentController.createStudent.bind(studentController));
+router.put('/students/:id',authMiddleware, studentController.updateStudent.bind(studentController));
+router.patch('/students/:id',authMiddleware, studentController.updateStudent.bind(studentController));
+router.delete('/students/:id',authMiddleware, studentController.deleteStudent.bind(studentController));
 
 
 export default router;
