c3fabe2e0e0f47333a90a71b2487f831fa8610c9
FRANÇOISE Anthonia <hei.anthonia@gmail.com>
2026-08-18 21:38:29 +0300

    feat: register authentication routes

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import studentsRoutes from "./routes/StudentsRoutes";
+import userRoutes from "./routes/UserRoute";
 
 dotenv.config();
 
@@ -10,6 +11,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 app.use('/crud', studentsRoutes );
+app.use('/auth', userRoutes);
 
 app.listen(PORT, () => {
     console.log(`serveur demarré sur http://localhost:${PORT}`)
